67d4e4a648f41d08e338eb49e837eb444d0fa549
Joao Paulo Vieira <vieira.joaopaulo@hotmail.com>
2017-04-27 13:43:59 -0300

    chore(build): add webpack config files

---
+++ webpack-base-config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  plugins: [
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(require('./package.json').version)
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+        // config in .babelrc
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['css', 'sass?includePaths[]='
+            + require('node-bourbon').includePaths
+            + '&includePaths[]='
+            + path.resolve(__dirname, './src/base/scss')
+        ],
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.(png|woff|eot|ttf|swf|cur)/, loader: 'url-loader?limit=1'
+      },
+      {
+        test: /\.html/, loader: 'html?minimize=false'
+      }
+    ],
+  },
+  resolve: {
+    root: path.resolve(__dirname, 'src'),
+    extensions: ['', '.js'],
+  }
+};
