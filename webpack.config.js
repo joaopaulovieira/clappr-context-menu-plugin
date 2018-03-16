4f7f4411d61bb292d1e9e258c4b8f569ebd84a39
Joao Paulo Vieira <vieira.joaopaulo@hotmail.com>
2018-03-16 01:20:11 -0300

    refactor(webpack.config.js): update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
 var path = require('path');
 var webpack = require('webpack');
-var Clean = require('clean-webpack-plugin');
 
-var webpackConfig = require("./webpack-base-config");
-webpackConfig.entry = path.resolve(__dirname, 'src/context_menu.js');
-
-if (process.env.npm_lifecycle_event === 'release') {
-  webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {warnings: false},
-    output: {comments: false}
-  }));
-} else {
-  webpackConfig.plugins.push(new Clean(['dist'], {verbose: false}));
+module.exports = {
+  externals: { Clappr : "Clappr" },
+  entry: path.resolve(__dirname, 'src/context_menu.js'),
+  plugins: [
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(require('./package.json').version)
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['css-loader', 'sass-loader?includePaths[]=' + path.resolve(__dirname, './src/public')],
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.html/,
+        loader: 'html-loader?minimize=false'
+      }
+    ],
+  },
+  resolve: { extensions: ['.js'] },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "latest/",
+    filename: 'clappr-context-menu-plugin.js',
+    library: 'ContextMenuPlugin',
+    libraryTarget: 'umd',
+  },
 }
-
-webpackConfig.externals = {
-  "Clappr": "Clappr",
-}
-
-webpackConfig.output = {
-  path: path.resolve(__dirname, 'dist'),
-  publicPath: '/',
-  filename: 'clappr-context-menu-plugin.js',
-  library: 'ContextMenuPlugin',
-  libraryTarget: 'umd',
-};
-
-module.exports = webpackConfig;
