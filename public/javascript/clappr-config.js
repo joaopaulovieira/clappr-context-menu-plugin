f39e750ce95bedbb474523d6a60788295e42e2ea
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-02-21 23:26:33 -0300

    refactor(public): remove unnecessary size config

--- public/javascript/clappr-config.js
+++ public/javascript/clappr-config.js
@@ -3,8 +3,6 @@ var playerElement = document.getElementById("player-wrapper");
 var player = new Clappr.Player({
   source: 'http://clappr.io/highline.mp4',
   poster: 'http://clappr.io/poster.png',
-  height: 360,
-  width: 640,
   plugins: [ContextMenuPlugin]
 });
 
