6e5216307258f21cded8c2a5051ab863cecc886b
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-01-26 21:59:41 -0300

    refactor(public): config demo page to publish on github pages

--- public/javascript/clappr-config.js
+++ public/javascript/clappr-config.js
@@ -3,7 +3,7 @@ const playerElement = document.getElementById('player-wrapper')
 const player = new Clappr.Player({
   source: 'http://clappr.io/highline.mp4',
   poster: 'http://clappr.io/poster.png',
-  playback: { controls: true },
+  // playback: { controls: true }, // use this config when test with the @clappr/core
   plugins: [window.ContextMenuPlugin],
 })
 
