3facfcf80db7b25307e863872fcb715b56605348
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-04-17 21:21:19 -0300

    Update demo page

--- javascript/clappr-config.js
+++
@@ -1,9 +0,0 @@
-const playerElement = document.getElementById('player-wrapper')
-
-const player = new Clappr.Player({
-  source: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-  playback: { controls: true },
-  plugins: [window.ContextMenuPlugin],
-})
-
-player.attachTo(playerElement)
