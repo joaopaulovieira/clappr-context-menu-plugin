6a77705258e77a8f7a499b5bab62500814eb53d3
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-04-17 21:10:26 -0300

    Update demo page

--- javascript/clappr-config.js
+++ javascript/clappr-config.js
@@ -1,9 +1,8 @@
 const playerElement = document.getElementById('player-wrapper')
 
 const player = new Clappr.Player({
-  source: 'http://clappr.io/highline.mp4',
-  poster: 'http://clappr.io/poster.png',
-  // playback: { controls: true }, // use this config when test with the @clappr/core
+  source: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+  playback: { controls: true },
   plugins: [window.ContextMenuPlugin],
 })
 
